2dc022e02e807718374b373b0a2d03f4284b51c4
mowtiie <mowtiie.dev@gmail.com>
2026-08-10 23:36:52 +0800

    Created activity and layout files for add edit secret activity

---
+++ app/src/main/java/com/mowtiie/keyheimer/ui/activities/AddEditSecretActivity.java
@@ -0,0 +1,26 @@
+package com.mowtiie.keyheimer.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.mowtiie.keyheimer.R;
+
+public class AddEditSecretActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_edit_secret);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
