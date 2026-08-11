32feed8ce74f8606f0c299fd40b740ba99d5839b
mowtiie <mowtiie.dev@gmail.com>
2026-08-12 04:32:33 +0800

    Created preference keys utility class

---
+++ app/src/main/java/com/mowtiie/keyheimer/util/PreferenceKeys.java
@@ -0,0 +1,25 @@
+package com.mowtiie.keyheimer.util;
+
+public final class PreferenceKeys {
+
+    private PreferenceKeys() {
+    }
+
+    public static final String KEY_THEME_MODE = "pref_theme_mode";
+    public static final String KEY_THEME_CONTRAST = "pref_theme_contrast";
+    public static final String KEY_DYNAMIC_COLOR = "pref_dynamic_color";
+    public static final String KEY_SECURITY_SCREEN = "pref_security_screen";
+    public static final String KEY_BIOMETRIC_ENABLED = "pref_biometric_enabled";
+    public static final String KEY_APP_LOCK = "pref_app_lock";
+    public static final String KEY_APP_LOCK_PIN_HASH = "pref_app_lock_pin_hash";
+    public static final String KEY_APP_LOCK_PIN_SALT = "pref_app_lock_pin_salt";
+    public static final String KEY_APP_LOCK_PIN_ITERATIONS = "pref_app_lock_pin_iterations";
+
+    public static final String THEME_MODE_LIGHT = "light";
+    public static final String THEME_MODE_DARK = "dark";
+    public static final String THEME_MODE_SYSTEM = "system";
+
+    public static final String CONTRAST_STANDARD = "standard";
+    public static final String CONTRAST_MEDIUM = "medium";
+    public static final String CONTRAST_HIGH = "high";
+}
