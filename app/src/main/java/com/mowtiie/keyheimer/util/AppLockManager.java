92e5faf9b319a841248d49b33758e23b858b21f9
mowtiie <mowtiie.dev@gmail.com>
2026-08-12 11:19:16 +0800

    Added app lock manager utility class

---
+++ app/src/main/java/com/mowtiie/keyheimer/util/AppLockManager.java
@@ -0,0 +1,55 @@
+package com.mowtiie.keyheimer.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+public final class AppLockManager {
+
+    private static volatile AppLockManager instance;
+
+    private boolean locked = true;
+
+    private AppLockManager() {
+    }
+
+    public static AppLockManager getInstance() {
+        if (instance == null) {
+            synchronized (AppLockManager.class) {
+                if (instance == null) {
+                    instance = new AppLockManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public boolean isLockRequired(Context context) {
+        return locked && isLockConfigured(context);
+    }
+
+    public boolean isLockConfigured(Context context) {
+        return hasPin(context) || isBiometricEnabled(context);
+    }
+
+    public boolean hasPin(Context context) {
+        return !prefs(context).getString(PreferenceKeys.KEY_APP_LOCK_PIN_HASH, "").isEmpty();
+    }
+
+    public boolean isBiometricEnabled(Context context) {
+        return prefs(context).getBoolean(PreferenceKeys.KEY_BIOMETRIC_ENABLED, false);
+    }
+
+    public void markUnlocked() {
+        locked = false;
+    }
+
+    public void markLocked() {
+        locked = true;
+    }
+
+    private SharedPreferences prefs(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+}
