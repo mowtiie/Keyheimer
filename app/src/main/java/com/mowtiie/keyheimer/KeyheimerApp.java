9f2c5eb20f222aa478b51398cc5b69c21721c5b2
mowtiie <mowtiie.dev@gmail.com>
2026-08-13 17:50:05 +0800

    Integrated crash handling in application class

--- app/src/main/java/com/mowtiie/keyheimer/KeyheimerApp.java
+++ app/src/main/java/com/mowtiie/keyheimer/KeyheimerApp.java
@@ -7,12 +7,14 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ProcessLifecycleOwner;
 
 import com.mowtiie.keyheimer.util.AppLockManager;
+import com.mowtiie.keyheimer.util.CrashHandler;
 
 public class KeyheimerApp extends Application implements DefaultLifecycleObserver {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        CrashHandler.install(this);
         ProcessLifecycleOwner.get().getLifecycle().addObserver(this);
     }
 
